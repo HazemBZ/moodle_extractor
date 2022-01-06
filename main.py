e95934315e2359ea8553c6fc648a2fa3d05838fc
HazemBZ <hazembenbz@gmail.com>
2022-01-06 16:46:21 +0100

    made tool interactivty instead of descriptive

--- main.py
+++ main.py
@@ -7,43 +7,63 @@
 import requests, os, sys
 from bs4 import BeautifulSoup as BS
 from tqdm import tqdm
+from PyInquirer import prompt
+import urllib3
+urllib3.disable_warnings()
 
+moodle_url = ""
+# login_url = ""
+email, password = "", ""
+course_url = ""
 
-url = ""
+DOWN_FOLDER = "saved"
 
 browser_cookie_string = os.environ.get("MOODLE_SESSION_COOKIE", None)
 
+q1 = {"type": "input", "name": "murl", "message": "Enter moodle platform URL"}
+q2 = {"type": "input", "name": "course", "message": "Enter course URL"}
+q3 = {"type": "input", "name": "email", "message": "Email"}
+q4 = {"type": "password", "name": "pass", "message": "Password"}
 
-if sys.argv[1]:
-    url = sys.argv[1]
-else:
-    print("no url specified")
-    sys.exit()
-
-
-if len(sys.argv) == 3:
-    browser_cookie_string = sys.argv[2]
-
-if not browser_cookie_string:
-    print('no moodle cookie on "MOODLE_SESSION_COOKIE" Env variable')
-    sys.exit(0)
-
-formated_cookies = dict(map(lambda s: s.split("="), browser_cookie_string.split("; ")))
-print(f"formatted cookie {formated_cookies}")
-
-res = requests.get(url, cookies=formated_cookies)
-
-soup = BS(res.text)
+questions = []
 
 
-# li.activity resource modtype_resource -> ... -> div.activityinstance
-# li#
-# topics list : >>> res1 = soup.find('ul', class_="topics")
-# topics items: >>> items = res1.find_all('li')
+if not moodle_url:
+  questions.append(q1)
+if not email:
+  questions.append(q3)
+if not password:
+  questions.append(q4)
 
-res1 = soup.find("ul", class_="topics")
-items = res1.find_all("li")
+answers = prompt(questions)
 
+if not moodle_url:
+  moodle_url = answers['murl']
+if not email:
+  email = answers['email']
+if not password:
+  password = answers['pass']
+
+s = requests.Session()
+res = s.get(moodle_url, verify=False, allow_redirects=True)
+soup = BS(res.text, features="lxml")
+loginToken = soup.find(attrs={'name': 'logintoken'})['value']
+post_url = soup.find(id="login")['action']
+
+data = {
+  'anchor': '',
+  'logintoken': loginToken,
+  'username': email,
+  'password': password,
+  'rememberusername': 1
+}
+
+
+p_res = s.post(post_url, data=data, verify=False,allow_redirects=True)
+    
+
+## loop for courses urls 
+print("logged in successfuly")
 
 def write_to_f(el):
     with open("f.txt", "w") as f:
@@ -54,19 +74,36 @@ def pprint(el):
     print(el.prettify())
 
 
-resource_links = list(filter(lambda x: "resource" in x.get("href"), res1.find_all("a")))
+if not os.access(DOWN_FOLDER, os.R_OK):
+  os.system(f"mkdir '{DOWN_FOLDER}'")
 
-for link in tqdm(resource_links):
 
-    resource_name = link.text
-    resource_source = link.get("href")
-    resource_extension = link.contents[0].get("src").split("/")[-1].split("-")[0]
-    if "power" in resource_extension:
-        resource_extension = "pptx"
+while True:
+  q = {'type': 'input', 'name':'course', 'message': 'Enter course url'}
+  course_url = prompt(q)['course']
 
-    print(f"\n\nDownloading {resource_name}: {resource_extension.upper()}")
-    resp = requests.get(resource_source, cookies=formated_cookies)
-    with open(f"{resource_name}.{resource_extension}", "wb") as f:
-        f.write(resp.content)
+  res = s.get(course_url, verify=False)
 
-print("Downloads done")
+  soup = BS(res.text, features="lxml")
+
+
+  res = soup.find("ul", class_="topics")
+  items = res.find_all("li")
+
+  resource_links = list(filter(lambda x: "resource" in x.get("href"), res.find_all("a")))
+
+  for link in tqdm(resource_links):
+
+      resource_name = link.text
+      resource_source = link.get("href")
+      resource_extension = link.contents[0].get("src").split("/")[-1].split("-")[0]
+      if "power" in resource_extension:
+          resource_extension = "pptx"
+
+      print(f"\n\nDownloading {resource_name}: {resource_extension.upper()}")
+      # resp = requests.get(resource_source, cookies=formated_cookies)
+      resp = s.get(resource_source)
+      with open(f"{os.path.join(DOWN_FOLDER, resource_name)}.{resource_extension}", "wb") as f:
+          f.write(resp.content)
+
+  print("Downloads done")
